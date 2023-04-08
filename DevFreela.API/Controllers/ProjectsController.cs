1919febfac2cb92b4a7d76614ba54148b8d68961
Adriano Fagundes <adriano.fagundes@datapar.com.br>
2023-04-08 19:14:37 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    public class ProjectsController : ControllerBase
+    {
+        [Route ("api/projects")]
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
